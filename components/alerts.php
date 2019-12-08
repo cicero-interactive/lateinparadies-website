55a54989624361a89b22d6a20817be2d9471eb3a
Cicero Interactive <burnerjulian@gmail.com>
2019-12-09 00:28:52 +0100

    Postponed release by one day

--- components/alerts.php
+++ components/alerts.php
@@ -22,7 +22,7 @@
 
                 <script>
                     // Set the date we're counting down to
-                    var countDownDate = new Date("Dec 09, 2019 02:00:00").getTime();
+                    var countDownDate = new Date("Dec 09, 2019 23:00:00").getTime();
 
                     // Update the count down every 1 second
                     var x = setInterval(function() {
