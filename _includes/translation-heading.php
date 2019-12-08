f68455af23b108c97a374523d8b3bc5015656123
Julian Burner <burnerjulian@gmail.com>
2019-12-08 21:29:06 +0100

    Fixed linking to poem sections when not appropriate

--- _includes/translation-heading.php
+++ _includes/translation-heading.php
@@ -6,22 +6,25 @@
 				<li><a href="#{{ chapter.id }}">{{ chapter.id-display }}</a></li>
 			{% endif %}
 
-			{% assign lastSpanOfSectionsBeginningIndex = 0 %}
-			{% assign lastSectionIndex = -2 %}
-			{% for section in chapter.sections %}
-				{% assign lastSectionIndex = lastSectionIndex | plus: 1 %}
-				{% if section.style == "verse-missing" or section.id == chapter.sections.last.id %}
-					{% if section.id == chapter.sections.last.id %}
-						{% assign lastSectionIndex = lastSectionIndex | plus: 1 %}
+			<!-- TODO: comments and clean-up -->
+			{% if page.chapters.size <= 1 %}
+				{% assign lastSpanOfSectionsBeginningIndex = 0 %}
+				{% assign lastSectionIndex = -2 %}
+				{% for section in chapter.sections %}
+					{% assign lastSectionIndex = lastSectionIndex | plus: 1 %}
+					{% if section.style == "verse-missing" or section.id == chapter.sections.last.id and section.style == "poem" %}
+						{% if section.id == chapter.sections.last.id %}
+							{% assign lastSectionIndex = lastSectionIndex | plus: 1 %}
+						{% endif %}
+						{% if chapter.sections[lastSectionIndex].id and chapter.sections[lastSectionIndex].id != "/" and chapter.sections[lastSectionIndex].id-display and chapter.sections[lastSectionIndex].id-display != "/" %}
+							<li><a href="#{{ chapter.sections[lastSpanOfSectionsBeginningIndex].id }}">
+								{{ chapter.sections[lastSpanOfSectionsBeginningIndex].id-display | append: "-" | append: chapter.sections[lastSectionIndex].id-display }}</a>
+							</li>
+						{% endif %}
+						{% assign lastSpanOfSectionsBeginningIndex = lastSectionIndex | plus: 2 %}
 					{% endif %}
-					{% if chapter.sections[lastSectionIndex].id and chapter.sections[lastSectionIndex].id != "/" and chapter.sections[lastSectionIndex].id-display and chapter.sections[lastSectionIndex].id-display != "/" %}
-						<li><a href="#{{ chapter.sections[lastSpanOfSectionsBeginningIndex].id }}">
-							{{ chapter.sections[lastSpanOfSectionsBeginningIndex].id-display | append: "-" | append: chapter.sections[lastSectionIndex].id-display }}</a>
-						</li>
-					{% endif %}
-					{% assign lastSpanOfSectionsBeginningIndex = lastSectionIndex | plus: 2 %}
-				{% endif %}
-			{% endfor %}
+				{% endfor %}
+			{% endif %}
 		{% endfor %}
 	</ul>
 </div>
