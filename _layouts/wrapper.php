18a9f450034394efa1f73e6a4fb521102d9364dc
Cicero Interactive <burnerjulian@gmail.com>
2019-12-18 17:14:02 +0100

    Added cookie_funcs

--- _layouts/wrapper.php
+++ _layouts/wrapper.php
@@ -12,6 +12,7 @@
 		<link rel="stylesheet" href="{{ site.baseurl }}/assets/css/stylesheet.css">
 		<meta name="viewport" content="width=device-width, initial-scale=0.70">
 		<meta name="Copyright" content="Copyright (c) 2016-<?php echo date("Y"); ?> by Cicero Interactive"/>
+		<script src="{{ site.baseurl }}/assets/js/cookie_funcs.js"></script>
 		<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 		<script src="{{ site.baseurl }}/assets/js/breadcrumb_cleaner.js"></script>-->
 		<meta charset="utf-8">
