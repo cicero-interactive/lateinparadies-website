10fa807fdea4089bfec1deb0138e04359e8a342f
Julian Burner <burnerjulian@gmail.com>
2019-12-24 13:46:41 +0100

    Use "du" instead of "Sie", cookie improvements

---
+++ Cicero/De-Re-Publica-(B1).php
@@ -0,0 +1,15 @@
+---
+layout: translation
+id: c9d63a110fd638137ce89bf2b94d1d22efe0a55a
+title: Cicero&#58; De Re Publica Übersetzung
+title_nav: De Re Publica
+heading: Cicero&#58; De Re Publica
+chapters:
+  - number: 42
+    latin: >-
+      ...
+    german: >-
+      Jede dieser drei Verfassungsformen - wenn die Menschen jenes Band zusammenhält, das sie zuerst unter sich gemeinschaftlich zu einem Staat verbunden hat - ist freilich nicht perfekt und nicht die Beste meiner Meinung nach ist, jedoch erträglich; und bei jeder Verfassungsform ist es so, dass die eine vortrefflicher sein könnte als die andere. Denn entweder ein gerechter und weiser König oder die Auserwählten und führenden Bürger oder das Volk selbst, obwohl dies am wenigsten zu billigen ist, diese Staatsformen scheinen dennoch von einigermaßen sicheren Zustand zu sein,wenn keine Ungerechtigkeiten und Begierden dazwischenkommen. Aber in den Monarchien haben die Übrigen allzu wenig Anteil am gemeinsamen Recht und der Entscheidung; und bei der Herrschaft der Optimaten kann die Menge kaum Anteil an Freiheit haben, weil sie jegliche gemeinsame Planung und Macht entbehrt; und wenn alles vom noch so gerechten und gemäßigten Volk ausgeführt wird, ist dennoch die Gleichheit selbst ungleich, dadurch, dass sie keine Abstufungen der Würde hat.
+source_latin: http://www.thelatinlibrary.com/cicero/ver.shtml
+counter: https://www.webfreecounter.com/hit.php?id=grxkxfd&nd=9&style=44
+---
--- _includes/alerts.php
+++ _includes/alerts.php
@@ -1,9 +1,9 @@
 <div id="alertCookies" class="alert alertYellow" style="position: sticky; top: 8px;">
-    <span class="closebtn" onclick="this.parentElement.style.display='none'; setCookie('closedAlertCookies', true, 1000);">&times;</span>
-    Um Ihnen ein besseres Nutzererlebnis zu bieten, verwenden wir Cookies. Durch Nutzung unserer Dienste stimmen Sie unserer Verwendung von Cookies zu. <a href="{{ site.baseurl }}/Datenschutzerklaerung.php">Weitere Informationen</a>
+    <span class="closebtn" onclick="this.parentElement.style.display='none'; setCookie('closedCookieAlert', true, 1000);">&times;</span>
+    Um euch ein besseres Nutzererlebnis zu bieten, verwenden wir Cookies. Durch Nutzung unserer Dienste stimmst du unserer Verwendung von Cookies zu. <a href="{{ site.baseurl }}/Datenschutzerklaerung.php">Weitere Informationen</a>
 </div>
 <script>
-    if (checkCookie("closedAlertCookies")) {
+    if (checkCookie("closedCookieAlert") && getCookie("closedCookieAlert") == "true") {
         document.getElementById("alertCookies").style.display = "none";
     }
 </script>
@@ -13,15 +13,16 @@
     durch deren selbstlosen Opfergaben wir das Lateinparadies für mindestens ein weiteres halbes Jahr konstant mit Updates und Fehlerverbesserungen versorgen können!
     Auflistung aller Spender:-->
     Auflistung aller Unterstützer des Lateinparadieses: <a href="{{ site.baseurl }}/Hall-of-Fame.php">Hall of Fame</a>.
-    Aktuelle News zum Lateinparadies gibt es  <a href="https://cicero-interactive.de.cool/news.php">hier</a>.
+    Aktuelle News zum Lateinparadies gibt es  <a href="https://cicero-interactive.de.cool/news.php" target="_blank">hier</a>.
 </div>
 <div class="alert alertRed">
     <!--<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>-->
     Durch ein technisches Problem wurden alle "ä"-, "ö"- und "ü"-Zeichen in Kleinbuchstaben konvertiert.
-    Trotz detaillierter Fehlersuche kann es sein, dass ein paar übersehen wurden. Sollte Dir ein solcher Fehler auffallen, melde ihn bitte auf <a href="https://github.com/Cicero-Interactive/Lateinparadies/issues">GitHub</a> oder kontaktiere uns unter unserer <a href="mailto:{{ site.email }}">E-Mail-Adresse</a>.
+    Trotz detaillierter Fehlersuche kann es sein, dass ein paar übersehen wurden. Sollte dir ein solcher Fehler auffallen, melde ihn bitte auf
+    <a href="https://github.com/Cicero-Interactive/Lateinparadies/issues" target="_blank">GitHub</a> oder kontaktiere uns unter unserer <a href="mailto:{{ site.email }}">E-Mail-Adresse</a>.
 </div>
 <div class="alert alertGreen">
     <!--<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>-->
-    Das Lateinparadies wurde großflächig geupdatet! Die Änderungen seht ihr <a href="https://cicero-interactive.de.cool/news.php">hier</a>.
-    Außerdem sind wir nun open-source! Verfolgt die Entwicklung auf <a href="https://github.com/Cicero-Interactive/Lateinparadies">GitHub</a>.
+    Das Lateinparadies wurde großflächig geupdatet! Die Änderungen seht ihr <a href="https://cicero-interactive.de.cool/news.php" target="_blank">hier</a>.
+    Außerdem sind wir nun open-source! Verfolgt die Entwicklung auf <a href="https://github.com/Cicero-Interactive/Lateinparadies" target="_blank">GitHub</a>.
 </div>
