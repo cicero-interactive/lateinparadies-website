9168269e9e66d68387a510637c1a804b728f4556
Julian Burner <burnerjulian@gmail.com>
2020-01-26 23:19:37 +0100

    Expanded Martialis description

--- Martial.php
+++ Martial.php
@@ -6,6 +6,6 @@ author: Martial
 author-fullname: Marcus Valerius Martialis
 dest_path: Martial/
 description: >- 
-  Marcus Valerius Martialis war ein römischer Dichter, der vor allem für seine Epigramme bekannt ist.
+  Wer sich mit dem Leben Martials beschäftigt, kann sich fast nur auf Angaben aus seinen Gedichten stützen. Jediglich in einem Plinius-Brief (Nachruf) findet man biographische Details. Martial stammt aus Spanien und dürfte nicht aus einer besonders wohlhabenden Familie stammen. Dazu passt auch, dass er relativ lange in seiner Heimatstadt die Schule besuchte und erst aufgrund herausragender Leistungen zur weiteren Ausbildung nach Rom geschickt wurde. Er beschreibt in seinen Gedichten auch sein ärmliches Leben, konnte sich aber mit 40 Jahren ein Landgut leisten, auf dem Sklaven arbeiteten. Dazu könnten auch Lobgedichte auf das flavische Kaiserhaus beigetragen haben. Nach deren Tod kehrte er wieder in seine Heimat zurück.
 counter: https://www.webfreecounter.com/hit.php?id=zmqoao&nd=9&style=44
 ---
