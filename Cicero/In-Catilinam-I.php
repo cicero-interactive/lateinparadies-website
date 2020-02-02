4b8ffea9ba76680202e9b2bb54b32cce22de527a
Julian Burner <burnerjulian@gmail.com>
2020-02-02 21:53:14 +0100

    Added In Catilinam I

---
+++ Cicero/In-Catilinam-I.php
@@ -0,0 +1,18 @@
+---
+layout: translation
+id: 7bf663305ff5039a60a7d7943f5f25b7717dd2bf
+title: Cicero&#58; In Catilinam I
+title_nav: In Catilinam I
+heading: Cicero&#58; In Catilinam I
+chapters:
+  - latin: >-
+      Quo usque tandem abutere, Catilina, patientia nostra? quam diu etiam furor iste tuus nos eludet? quem ad finem sese effrenata iactabit audacia? Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae, nihil timor populi, nihil concursus bonorum omnium, nihil hic munitissimus habendi senatus locus, nihil horum ora voltusque moverunt? Patere tua consilia non sentis, constrictam iam horum omnium scientia teneri coniurationem tuam non vides? Quid proxima, quid superiore nocte egeris, ubi fueris, quos convocaveris, quid consilii ceperis, quem nostrum ignorare arbitraris?
+    german: >-
+      Wie lange noch, Catilina, willst du unsere Geduld missbrauchen? Wie lange noch wird uns dieser sein Wahnsinn da verspotten? Bis zu welchem Ende wird sich die zügellose Dreistigkeit vorwagen? Bewegten dich nicht etwa die nächtliche Bewachung des Palatin, die Nachtwache der Stadt, die Furcht des Volkes, die Zusammenkunft aller guten Menschen, dieser überaus befestigte Ort des zu haltenden Senates und die Gesichter und Mienen von diesen? Fühlst du nicht, dass deine Pläne offenstehen? Siehst du nicht, dass deine Verschwörung durch das Wissen all dieser lahm gelegt ist? Glaubst du, dass irgend jemand von uns nicht weiß, was du letzte, was du vorletzte Nacht gemacht hast, wo du warst, wen du zusammengerufen hast, welchen Plan du gefasst hast? 
+  - latin: >-
+      O tempora, o mores! Senatus haec intellegit. Consul videt; hic tamen vivit. Vivit? immo vero etiam in senatum venit, fit publici consilii particeps, notat et designat oculis ad caedem unum quemque nostrum. Nos autem fortes viri satis facere rei publicae videmur, si istius furorem ac tela vitemus. Ad mortem te, Catilina, duci iussu consulis iam pridem oportebat, in te conferri pestem, quam tu in nos [omnes iam diu] machinaris. 
+    german: >-
+      O Zeiten, o Sitten! Der Senat weiß es, der Konsul sieht es; dennoch lebt dieser. Er lebt? Schlimmer noch, er kommt sogar in den Senat, nimmt an der öffentlichen Versammlung teil, bestimmt und kennzeichnet mit seinen Augen einen der Unseren zum Tod. Wir tapferen Männer aber scheinen genug für den Staat zu tun, wenn wir die Wut und die Geschosse dessen da meiden. Es hätte sich schon längst gehört, Catilina, dass du auf Befehl des Konsuls zum Tode geführt wirst und dass dir das Unheil geschieht, das du schon lange gegen uns alle planst.
+source_latin: http://www.thelatinlibrary.com/cicero/cat1.shtml
+counter: https://www.webfreecounter.com/hit.php?id=gmxqnfa&nd=9&style=44
+---
