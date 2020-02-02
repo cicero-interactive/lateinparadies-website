75fb8420cb53edcc33edb8857a75af847c4ab4c2
Julian Burner <burnerjulian@gmail.com>
2020-02-02 21:56:57 +0100

    Added Ovid's Metamorphoses

---
+++ Ovid/Metamorphosen-(B1).php
@@ -0,0 +1,23 @@
+---
+layout: translation
+id: 70998a80f761a9dc7429a7b0aaedb681039d0b04
+title: Ovid&#58; Metamorphosen (Buch I)
+title_nav: Metamorphosen (Buch I)
+heading: Ovid&#58; Metamorphosen
+chapters:
+  - number: 0
+    style: poem
+    sections:
+      - latin: |
+          In nova fert animus mutatas dicere formas
+          corpora; di, coeptis (nam vos mutastis et illas)
+          adspirate meis primaque ab origine mundi
+          ad mea perpetuum deducite tempora carmen!
+        german: |
+          Mein Sinn drängt mich, Gestalten zu besingen, die in neue Körpern verwandelt wurden;
+          Götter, begünstigt mein Vorhaben (denn ihr habt auch jene verändert)
+          und führt das Gedicht ununterbrochen
+          vom Beginn der Welt bis zu meiner Zeit!
+source_latin: http://www.thelatinlibrary.com/ovid/ovid.met1.shtml
+counter: https://www.webfreecounter.com/hit.php?id=gxqnoo&nd=9&style=44
+---
---
+++ Ovid/Metamorphosen-(B4).php
@@ -0,0 +1,125 @@
+---
+layout: translation
+id: e3a584d95b5ff5957fa05a2c2cfd09240fe6d68d
+title: Ovid&#58; Metamorphosen (Buch IV)
+title_nav: Metamorphosen (Buch IV)
+heading: Ovid&#58; Metamorphosen
+chapters:
+  - number: /
+    style: poem
+    sections:
+      - number: 55
+        latin: |
+          Pyramus et Thisbe, iuvenum pulcherrimus alter,
+          altera, quas Oriens habuit, praelata puellis,
+          contiguas tenuere domos, ubi dicitur altam
+          coctilibus muris cinxisse Semiramis urbem.
+          notitiam primosque gradus vicinia fecit,
+          tempore crevit amor; taedae quoque iure coissent,
+          sed vetuere patres: quod non potuere vetare,
+          ex aequo captis ardebant mentibus ambo.
+          conscius omnis abest; nutu signisque loquuntur,
+          quoque magis tegitur, tectus magis aestuat ignis.
+          fissus erat tenui rima, quam duxerat olim,
+          cum fieret, paries domui communis utrique.
+          id vitium nulli per saecula longa notatum -
+          quid non sentit amor? - primi vidistis amantes
+          et vocis fecistis iter, tutaeque per illud
+          murmure blanditiae minimo transire solebant.
+          saepe, ubi constiterant hinc Thisbe, Pyramus illinc,
+          inque vices fuerat captatus anhelitus oris,
+          "invide" dicebant "paries, quid amantibus obstas?
+          quantum erat, ut sineres toto nos corpore iungi
+          aut, hoc si nimium est, vel ad oscula danda pateres?
+          nec sumus ingrati: tibi nos debere fatemur,
+          quod datus est verbis ad amicas transitus auris."
+        german: |
+          Pyramus und Thisbe, der eine der schönste Jüngling,
+          die andere vorgezogen <span class="annotation">[sogar]</span> den Mädchen, die der Orient hatte,
+          bewohnten benachbarte Häuser, wo Semiramis die hohe Stadt
+          mit Mauern aus Tonziegeln errichtet haben soll.
+          Die Nachbarschaft bewirkte <span class="annotation">[deren]</span> Bekanntschaft und erste Schritte,
+          und mit der Zeit wuchs die Liebe. Auch die Hochzeitsfackeln hätten mit Recht zusammengehen können,
+          aber die Väter haben es verboten. Was sie nicht verbieten konnten:
+          Beide brannten gleichermaßen, nachdem ihre Sinne <span class="annotation">[von der Liebe]</span> erfasst worden waren.
+          Jeder Mitwisser war abwesend; sie sprachen durch Zunicken und Zeichen,
+          und je mehr das Feuer bedeckt wurde, desto mehr loderte es auf.
+          Gespalten war die gemeinsame Trennwand beider Häuser von einem dünnen Ritz,
+          der einst beim Bau der Mauer entstanden war.
+          Diesen über lange Jahrhunderte von niemandem bemerkten Fehler habt - 
+          was bemerkt die Liebe nicht? - als erste ihr Liebenden gesehen
+          und habt ihn zu einem Weg für eure Stimme gemacht;
+          und sicher pflegten die Schmeicheleien in Form von leisestem Gemurmel durch jenen hinüberzugehen.
+          Oft, sobald sie dastanden, hier Thisbe, dort Pyramus,
+          und gegenseitig der Atem des Mundes gefangen worden war,
+          sagten sie: „Neidische Mauer, was hinderst Du die Liebenden?
+          Wie viel wäre schon dabei, wenn du zulassen würdest, dass wir uns am ganzen Körper vereinen,
+          oder, wenn das zu viel ist, du wenigstens zum Austausch von Küssen offenstehen würdest?
+          Aber wir sind nicht undankbar; wir geben zu, dass wir dir Dank schulden,
+          weil du den Worten einen Zugang zu den befreundeten Ohren geschaffen hast."
+      - style: verse-seperator
+      - latin: |
+          talia diversa nequiquam sede locuti
+          sub noctem dixere "vale" partique dedere
+          oscula quisque suae non pervenientia contra.
+          postera nocturnos Aurora removerat ignes,
+          solque pruinosas radiis siccaverat herbas:
+          ad solitum coiere locum. tum murmure parvo
+          multa prius questi statuunt, ut nocte silenti
+          fallere custodes foribusque excedere temptent,
+          cumque domo exierint, urbis quoque tecta relinquant,
+        german: |
+          Nachdem sie so vergeblich auf getrennten Plätzen sitzend gesprochen hatten,
+          sagten sie bei Einbruch der Nacht „Auf Wiedersehen“ und gaben ein jeder
+          seinen Teil Küsse, die nicht auf die Gegenseite gelangten.
+          Das folgende Sonnenlicht hatte die nächtlichen Feuer entfernt,
+          und die Sonne hatte die taufeuchten Kräuter mit Sonnenstrahlen getrocknet.
+          Sie trafen sich am gewohnten Platz. Dann beschlossen sie mit leisem Geflüster,
+          nachdem sie sich zuvor über vieles beklagt hatten, dass sie versuchen würden,
+          in der nächtlichen Stille die Wächter zu täuschen und aus dem Haus hinauszugehen,
+          dass sie auch die Dächer der Stadt verlassen, wenn sie aus dem Haus gehen.
+      - style: verse-seperator
+      - latin: |
+          neve sit errandum lato spatiantibus arvo,
+          conveniant ad busta Nini lateantque sub umbra
+          arboris: arbor ibi niveis uberrima pomis,
+          ardua morus, erat, gelido contermina fonti.
+          pacta placent; et lux, tarde discedere visa,
+          praecipitatur aquis, et aquis nox exit ab isdem. 
+        german: |
+          Damit sich die Umhergehenden nicht auf dem weiten Feld verirren,
+          beschließen sie, sich am Grab des Ninus zu treffen
+          und sich im Schatten des Baumes zu verstecken. Der Baum dort war ein hoher Maulbeerbaum,
+          sehr reich an schneeweißen Früchten und benachbart einer kühlen Quelle.
+          Die Pläne gefallen und das Licht schien langsam zu weichen.
+          Die Sonne taucht ins Wasser hinein und die Nacht steigt aus demselben Wasser herauf.
+      - style: verse-seperator
+      - latin: |
+          Callida per tenebras versato cardine Thisbe
+          egreditur fallitque suos adopertaque vultum
+          pervenit ad tumulum dictaque sub arbore sedit.
+          audacem faciebat amor. venit ecce recenti
+          caede leaena boum spumantis oblita rictus
+          depositura sitim vicini fontis in unda;
+          quam procul ad lunae radios Babylonia Thisbe
+          vidit et obscurum timido pede fugit in antrum,
+          dumque fugit, tergo velamina lapsa reliquit.
+          ut lea saeva sitim multa conpescuit unda,
+          dum redit in silvas, inventos forte sine ipsa
+          ore cruentato tenues laniavit amictus.
+        german: |
+          Nachdem sie die Tür geöffnet hatte, ging die schlaue Thisbe
+          in die Dunkelheit hinaus, täuschte die ihren, gelangte mit verhülltem
+          Gesicht zum Hügel und setzte sich unter den vereinbarten Baum.
+          Die Liebe machte sie mutig. Schau, da kommt eine Löwin mit schäumendem Maul,
+          das noch besudelt ist vom frischen Blut eines kürzlich getöteten Rindes,
+          um ihren Durst im Wasser der benachbarten Quelle zu stillen.
+          Die babylonische Thisbe sah diese von der Ferne im Mondlicht,
+          floh ängstlichen Fußes in die dunkle Höhle und verlor ihren Schleier,
+          der ihr vom Rücken geglitten war.
+          Als die wilde Löwin ihren Durst mit viel Wasser gestillt hatte,
+          zerfetzte sie mit blutbespritztem Maul den dünnen Umhang,
+          den sie zufällig und ohne Thisbe selbst gefunden hatte, während sie in die Wälder zurückkehrte.
+source_latin: https://www.thelatinlibrary.com/ovid/ovid.met4.shtml
+counter: https://www.webfreecounter.com/hit.php?id=gvxqnop&nd=9&style=44
+---
