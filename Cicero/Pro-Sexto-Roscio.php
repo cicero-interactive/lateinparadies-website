36dd9a0e1d1f63e23439d60ce0f4b43bb525ada6
Julian Burner <burnerjulian@gmail.com>
2020-02-02 11:25:22 +0100

    Fixed wrongly allocated translation

--- Cicero/Pro-Sexto-Roscio.php
+++ Cicero/Pro-Sexto-Roscio.php
@@ -4,12 +4,7 @@ id: a050c4ded6f86c519cd71671d5c60cd514d669b5
 title: Cicero&#58; Pro Sextio Roscio Übersetzung
 title_nav: Pro Sextio Roscio
 heading: Cicero&#58; Pro Sextio Roscio
-chapters:
-  - number: 46
-    latin: >-
-      Delum venit. Ibi ex fano Apollinis religiosissimo noctu clam sustulit signa pulcherrima atque antiquissima, eaque in onerariam navem. suam conicienda curavit. Postridie cum fanum spoliatum viderent ii qui Delum incolebant, graviter ferebant; est enim tanta apud eos eius fani religio atque antiquitas ut in eo loco ipsum Apollinem natum esse arbitrentur. Verbum tamen facere non audebant, ne forte ea res ad Dolabellam ipsum pertineret. Tum subito tempestates coortae sunt maximae, iudices, ut non modo proficisci cum cuperet Dolabella non posset sed vix in oppido consisteret: ita magni fluctus eiciebantur. Hic navis illa praedonis istius, onusta signis religiosis, expulsa atque eiecta fluctu frangitur; in litore signa illa Apollinis reperiuntur; iussu Dolabellae reponuntur. Tempestas sedatur, Dolabella Delo proficiscitur. 
-    german: >-
-      Er kam nach Delos. Dort hat er aus einem sehr ehrwürdigen Tempel Apolls heimlich in der Nacht sehr schöne und uralte Standbilder beseitigt und ließ diese auf sein Lastschiff bringen. Als am Tag darauf die Bewohner von Delos den ausgeplünderten Tempel sahen, ertrugen sie es schwer. Denn die religöse Verehrung und das hohe Alter dieses Tempels ist bei ihnen so groß, dass sie glauben, dass Apoll selbst an diesem Ort geboren sei. Dennoch wagten sie kein Wort zu äußern, aus Besorgnis, die Sache möchte vielleicht Dolabella selbst berühren. Dann erhoben sich sofort sehr große Unwetter, Richter, dass nicht nur nicht Dolabella,	obwohl er wollte, aufbrechen konnte, sondern kaum in der Stadt bleiben konnte: solch heftige Wellen wurden [von der See] ausgeworfen. Da wurde jenes Schiff dieses Räubers, das mit den heiligen Standbildern beladen war,  hinausgeworfen und vertrieben und von der Flut zerbrochen; an der Küste wurden jene Statuen des Apolls wiedergefunden; auf Dolabellas Befehl wurden sie zurückgestellt. Der Sturm legt sich, Dolabella reiste aus Delos ab.
+
 source_latin: http://www.thelatinlibrary.com/cicero/ver.shtml
 counter: https://www.webfreecounter.com/hit.php?id=grxkxfd&nd=9&style=44
 ---
